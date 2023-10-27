9c8908bf328de47ab4872e4e8f6afb4263868c5f
Alex <ajcbrown820@gmail.com>
2023-10-27 16:28:10 +1100

    Add parser

--- lox/src/main/java/lox/Lox.java
+++ lox/src/main/java/lox/Lox.java
@@ -43,8 +43,16 @@ public class Lox {
         }
     }
 
-    public static void error(int line, String message) {
+    public static void error(Integer line, String message) {
         report(line, "", message);
+    }
+
+    public static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
     private static void report(int line, String where, String message) {
@@ -56,8 +64,13 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError)
+            return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 }
